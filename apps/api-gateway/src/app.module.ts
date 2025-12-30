57cdb77ca36bc4040e67ee9a5f007cb1504c730c
rabbaniha <heydarrabbaniha@gmail.com>
2025-12-30 14:31:27 +0330

    feat(config): integrate SharedConfigModule and enhance environment validation schema

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedConfigModule } from '@app/config';
 
 @Module({
-  imports: [],
+  imports: [SharedConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/config/src/env.validation.ts
+++ libs/config/src/env.validation.ts
@@ -4,5 +4,7 @@ export const envSchema = Joi.object({
   NODE_ENV: Joi.string().required(),
   API_PORT: Joi.number(),
   AUTH_PORT: Joi.number(),
+  FARM_PORT: Joi.number(),
+  SUBSCRIPTION_PORT: Joi.number(),
   DB_HOST: Joi.string().required(),
 });
